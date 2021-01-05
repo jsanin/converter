910cdb9ab75a2a2fe4c015f03e5c05d871efd294
juansanin <juansanin@thepublicgroup.com>
2021-01-05 16:39:03 -0500

    Update folder for general integration test for converter api

--- converter-api/src/integration-test/java/com/jsanin/takehome/converter/ConverterApplicationIT.java
+++ converter-api/src/integration-test/java/com/jsanin/takehome/converter/ConverterApplicationIT.java
@@ -3,8 +3,8 @@ package com.jsanin.takehome.converter;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class ConverterApplicationTests {
+@ConverterSpringBootTest
+class ConverterApplicationIT {
 
 	@Test
 	void contextLoads() {
